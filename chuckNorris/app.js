7a5d2cbf0b2a7d7b35919110986bec2ffa780631
Christina Landvytovych <Crofty.land@gmail.com>
2018-09-23 21:08:06 +0300

    fixed bugs

--- chuckNorris/app.js
+++ chuckNorris/app.js
@@ -1,29 +1,31 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
-    const number = document.querySelector('input [type = "number"]').value;
+  const number = document.querySelector('input[type="number"]').value;
 
-    const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-    xhr.onload = function () {
-        if (this.status === 200) {
-            const response = JSON.parse(this.responseText);
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      
+      let output = '';
 
-            let output = '';
+      if(response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
 
-            if (response.type === 'success') {
-                response.value.forEach(function (joke) {
-                    output += '<li>${joke.joke}</li>';
-                })
-            } else {
-                output += '<li>Something go wrong</li>';
-            }
-            document.querySelector('.jokes').innerHTML = output;
-        }
-    };
-    xhr.send();
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
 
-    e.preventDefault();
+  xhr.send();
+
+  e.preventDefault();
 }
