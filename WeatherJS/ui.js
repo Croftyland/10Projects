2cc23b09eb63b2a2cdd8a911194ad681e065ded8
Christina Landvytovych <Crofty.land@gmail.com>
2018-09-29 02:05:21 +0300

    constructor UI

--- WeatherJS/ui.js
+++ WeatherJS/ui.js
@@ -1,22 +1,24 @@
-class Weather {
-    constructor(city, state) {
-        this.apiKey = '99dfe35fcb7de1ee';
-        this.city = city;
-        this.state = state;
+class UI {
+    constructor() {
+        this.location = document.getElementById('w-location');
+        this.desc = document.getElementById('w-desc');
+        this.string = document.getElementById('w-string');
+        this.details = document.getElementById('w-details');
+        this.icon = document.getElementById('w-icon');
+        this.humidity = document.getElementById('w-humidity');
+        this.feelsLike = document.getElementById('w-feels-like');
+        this.dewpoint= document.getElementById('w-dewpoint');
+        this.wind = document.getElementById('w-wind');
     }
 
-    // Fetch weather from API
-    async getWeather() {
-        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
-
-        const responseData = await response.json();
-
-        return responseData.current_observation;
-    }
-
-    // Change weather location
-    changeLocation(city, state) {
-        this.city = city;
-        this.state = state;
+    paint(weather) {
+        this.location.textContent = weather.display_location.full;
+        this.desc.textContent = weather.weather;
+        this.string.textContent = weather.temperature_string;
+        this.icon.setAttribute('src', weather.icon_url);
+        this.humidity.textContent = `Relative Humidity: ${weather.relative_humidity}`;
+        this.feelsLike.textContent = `Feels Like: ${weather.feelslike_string}`;
+        this.dewpoint.textContent = `DewPoint: ${weather.dewpoint_string}`;
+        this.wind.textContent = `Wind: ${weather.wind_string}`;
     }
 }
